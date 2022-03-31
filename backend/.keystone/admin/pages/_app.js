9d4d4b894326b5689196f3b42ec5a5f17118cc2e
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-03-31 19:44:12 +0100

    backend stepped solutions

---
+++ backend/.keystone/admin/pages/_app.js
@@ -0,0 +1,211 @@
+import React from 'react';
+
+import { KeystoneProvider } from '@keystone-next/admin-ui/context';
+import { ErrorBoundary } from '@keystone-next/admin-ui/components';
+import { Core } from '@keystone-ui/core';
+
+import * as viewb0c85232 from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
+import * as view527cda12 from '../../../node_modules/@keystone-next/fields/types/text/views';
+import * as viewe85428f8 from '../../../node_modules/@keystone-next/fields/types/password/views';
+import * as view16401885 from '../../../node_modules/@keystone-next/fields/types/timestamp/views';
+import * as view45b97414 from '../../../node_modules/@keystone-next/fields/types/relationship/views';
+import * as viewbe0bb718 from '../../../node_modules/@keystone-next/fields/types/select/views';
+import * as viewb3977b11 from '../../../node_modules/@keystone-next/fields/types/integer/views';
+import * as view72c9138c from '../../../node_modules/@keystone-next/cloudinary/views';
+
+const adminConfig = {};
+
+const fieldViews = {
+  viewb0c85232,
+  view527cda12,
+  viewe85428f8,
+  view16401885,
+  view45b97414,
+  viewbe0bb718,
+  viewb3977b11,
+  view72c9138c,
+};
+
+const lazyMetadataQuery = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: {
+              kind: 'Name',
+              value: 'keystone',
+              loc: { start: 22, end: 30 },
+            },
+            arguments: [],
+            directives: [],
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'Field',
+                  name: {
+                    kind: 'Name',
+                    value: 'adminMeta',
+                    loc: { start: 39, end: 48 },
+                  },
+                  arguments: [],
+                  directives: [],
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      {
+                        kind: 'Field',
+                        name: {
+                          kind: 'Name',
+                          value: 'lists',
+                          loc: { start: 59, end: 64 },
+                        },
+                        arguments: [],
+                        directives: [],
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            {
+                              kind: 'Field',
+                              name: {
+                                kind: 'Name',
+                                value: 'key',
+                                loc: { start: 77, end: 80 },
+                              },
+                              arguments: [],
+                              directives: [],
+                              loc: { start: 77, end: 80 },
+                            },
+                            {
+                              kind: 'Field',
+                              name: {
+                                kind: 'Name',
+                                value: 'isHidden',
+                                loc: { start: 91, end: 99 },
+                              },
+                              arguments: [],
+                              directives: [],
+                              loc: { start: 91, end: 99 },
+                            },
+                            {
+                              kind: 'Field',
+                              name: {
+                                kind: 'Name',
+                                value: 'fields',
+                                loc: { start: 110, end: 116 },
+                              },
+                              arguments: [],
+                              directives: [],
+                              selectionSet: {
+                                kind: 'SelectionSet',
+                                selections: [
+                                  {
+                                    kind: 'Field',
+                                    name: {
+                                      kind: 'Name',
+                                      value: 'path',
+                                      loc: { start: 131, end: 135 },
+                                    },
+                                    arguments: [],
+                                    directives: [],
+                                    loc: { start: 131, end: 135 },
+                                  },
+                                  {
+                                    kind: 'Field',
+                                    name: {
+                                      kind: 'Name',
+                                      value: 'createView',
+                                      loc: { start: 148, end: 158 },
+                                    },
+                                    arguments: [],
+                                    directives: [],
+                                    selectionSet: {
+                                      kind: 'SelectionSet',
+                                      selections: [
+                                        {
+                                          kind: 'Field',
+                                          name: {
+                                            kind: 'Name',
+                                            value: 'fieldMode',
+                                            loc: { start: 175, end: 184 },
+                                          },
+                                          arguments: [],
+                                          directives: [],
+                                          loc: { start: 175, end: 184 },
+                                        },
+                                      ],
+                                      loc: { start: 159, end: 198 },
+                                    },
+                                    loc: { start: 148, end: 198 },
+                                  },
+                                ],
+                                loc: { start: 117, end: 210 },
+                              },
+                              loc: { start: 110, end: 210 },
+                            },
+                          ],
+                          loc: { start: 65, end: 220 },
+                        },
+                        loc: { start: 59, end: 220 },
+                      },
+                    ],
+                    loc: { start: 49, end: 228 },
+                  },
+                  loc: { start: 39, end: 228 },
+                },
+              ],
+              loc: { start: 31, end: 234 },
+            },
+            loc: { start: 22, end: 234 },
+          },
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'authenticatedItem' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'InlineFragment',
+                  typeCondition: {
+                    kind: 'NamedType',
+                    name: { kind: 'Name', value: 'User' },
+                  },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Core>
+      <KeystoneProvider
+        adminConfig={adminConfig}
+        adminMetaHash="1c59zf7"
+        fieldViews={fieldViews}
+        lazyMetadataQuery={lazyMetadataQuery}
+      >
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+      </KeystoneProvider>
+    </Core>
+  );
+}
---
+++ backend/keystone.ts
@@ -0,0 +1,67 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long they are signed in for?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('connected to the database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // schema goes in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // TODO change this for roles
+      // show UI to only people that have passed test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    // TODO add session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  })
+);
